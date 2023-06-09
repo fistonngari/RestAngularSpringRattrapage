b13a5efbaba9634877d5f7a73ee73bd48a2aceaa
fistonngari <136085404+fistonngari@users.noreply.github.com>
2023-06-09 14:41:18 +0000

    Delete loudiGlrsSpringEndPoint directory

--- loudiGlrsSpringEndPoint/src/main/java/ism/etudiant/api/exceptions/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ism.etudiant.api.exceptions;
-
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
--- loudiGlrsSpringEndPoint/src/main/java/ism/etudiant/entities/AppUser.java
+++
@@ -1,5 +0,0 @@
-package ism.etudiant.entities;
-
-public class AppUser {
-
-}
